323fd9470631d0e1e139825e109690e4cca9f350
Sneha <snehaarora68791@gmail.com>
2025-01-08 10:55:14 +0530

    ok

---
+++ Jan08.java
@@ -0,0 +1,20 @@
+
+// Problem 3042. Count Prefix and Suffix Pairs I
+
+public class Jan08 {
+    public int countPrefixSuffixPairs(String[] words) {
+        int n = words.length ;
+        int count = 0 ; 
+        for(int i = 0 ; i< n; i++){
+            for(int j = i+1 ; j<n ; j++){
+                String s1 = words[i] ;
+                String s2 = words[j] ;
+                if(s1.length() > s2.length()) continue ; 
+                if(s2.startsWith(s1) && s2.endsWith(s1)){
+                    count ++ ; 
+                }
+            }
+        }
+        return count ; 
+    }
+}
